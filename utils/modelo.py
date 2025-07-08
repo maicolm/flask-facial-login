0594f755be271978368400861621d06134dd8004
Maicolm Rivera <sistemas_mr@hotmail.com>
2025-07-08 17:11:01 -0500

    Agrega función reconocer_rostro_con_modelo

--- utils/modelo.py
+++ utils/modelo.py
@@ -17,3 +17,17 @@ def cargar_modelo_logistico():
     if os.path.exists(MODELO_LOGISTICO_PATH):
         return joblib.load(MODELO_LOGISTICO_PATH)
     return None
+
+def reconocer_rostro_con_modelo(img):
+    modelo = cargar_modelo_logistico()
+    if modelo is None:
+        return None
+
+    try:
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_flat = img_gray.flatten().reshape(1, -1)
+        id_predicho = modelo.predict(img_flat)[0]
+        return id_predicho
+    except Exception as e:
+        print(f"Error al reconocer el rostro: {e}")
+        return None
